bd489882462121b344ab2180f66fc22f873ba4c1
Ga Fa <gafa@outlook.de>
2021-04-26 14:36:42 +0200

    Test

--- Software/GebaeudeManagement/Client/Client.h
+++ Software/GebaeudeManagement/Client/Client.h
@@ -4,7 +4,7 @@ class Client
 {
 public:
 	Client(char server[], char port[]);
-	void sendRequest(const char request[], char answer[]);
+	void sendRequest(const char request[],  char answer[]);
 	~Client();
 protected:
 
